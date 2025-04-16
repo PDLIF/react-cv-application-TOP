5cf58fb9b74d197e722e8dc2bfa8e6bfa634a7bf
astuarykig <akvsevolod@icloud.com>
2025-04-16 14:37:28 +0500

    add imports of GeneralInfo, Education and Experience components into App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-
+import GeneralInfo from './components/generalInfo/GeneralInfo';
+import Education from './components/education/Education';
+import Experience from './components/experience/Experience';
 
 function App() {
 
   return (
     <div className='App'>
       <h1>CV Builder</h1>
+      <GeneralInfo />
+      <Education />
+      <Experience />
     </div>
   );
 }
