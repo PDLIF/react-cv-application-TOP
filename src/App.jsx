0216a5fc52d4616ba227d6aec3b5edcf35df9fb2
astuarykig <akvsevolod@icloud.com>
2025-04-18 17:45:46 +0500

    feat: connect Education component to CV data management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import useCVData from './hooks/useCVData';
 import GeneralInfo from './components/generalInfo/GeneralInfo';
 import Education from './components/education/Education';
 import Experience from './components/experience/Experience';
 
 function App() {
+  const {
+    cvData,
+    updatePersonalInfo,
+    addEducation,
+    addExperience,
+    updateEducation,
+    updateExperience,
+    removeEducation,
+    removeExperience,
+  } = useCVData();
 
   return (
     <div className='App'>
       <h1>CV Builder</h1>
-      <GeneralInfo />
-      <Education />
+      <GeneralInfo
+        personalInfo={cvData.personalInfo}
+        onPersonalInfoChange={updatePersonalInfo}
+      />
+      <Education education={cvData.education} onAdd={addEducation} onUpdate={updateEducation} onRemove={removeEducation} />
       <Experience />
     </div>
   );
