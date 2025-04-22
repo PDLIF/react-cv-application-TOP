5f920018989174347c924656e388403afce599bd
astuarykig <akvsevolod@icloud.com>
2025-04-22 15:24:18 +0500

    feat: link CVPrint component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import useCVData from './hooks/useCVData';
 import GeneralInfo from './components/generalInfo/GeneralInfo';
 import Education from './components/education/Education';
 import Experience from './components/experience/Experience';
+import CVPreview from './components/other/CVPreview';
 
 function App() {
   const {
@@ -21,6 +22,7 @@ function App() {
       <GeneralInfo personalInfo={cvData.personalInfo} onPersonalInfoChange={updatePersonalInfo} />
       <Education education={cvData.education} onAdd={addEducation} onUpdate={updateEducation} onRemove={removeEducation} />
       <Experience experience={cvData.experience} onAdd={addExperience} onUpdate={updateExperience} onRemove={removeExperience} />
+      <CVPreview generalInfo={cvData.personalInfo} education={cvData.education} experience={cvData.experience} />
     </div>
   );
 }
