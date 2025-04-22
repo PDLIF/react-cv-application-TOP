0fa824616b041b45e77d0406779e69a8125bb548
astuarykig <akvsevolod@icloud.com>
2025-04-22 15:20:14 +0500

    feat: create a component that prints general info

---
+++ src/components/generalInfo/GeneralInfoPrint.jsx
@@ -0,0 +1,30 @@
+function GeneralInfoPrint({ fullName, email, phone, adress }) {
+    return(
+        <div className="general-info">
+            <h1 className="resume-name">{fullName}</h1>
+            <div className="contact-info">
+                {email && (
+                    <div>
+                        <i className="fa-colid fa-envelope" />
+                        <span>{email}</span>
+                    </div>
+                )}
+
+                {phone && (
+                    <div>
+                        <i className="fa-solid fa-phone" />
+                        <span>{phone}</span>
+                    </div>
+                )}
+
+                {adress && (
+                    <div>
+                        <i className="fa-solid fa-location-dot" />
+                        <span>{adress}</span>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+export default GeneralInfoPrint;
