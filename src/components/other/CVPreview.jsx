9f2f57c3b9c5d7c7aadabe79ea5621b62246a60e
astuarykig <akvsevolod@icloud.com>
2025-04-23 14:16:06 +0500

    feat: link new ExperiencePrint component to CVPreview component

--- src/components/other/CVPreview.jsx
+++ src/components/other/CVPreview.jsx
@@ -1,9 +1,13 @@
 import GeneralInfoPrint from "../generalInfo/GeneralInfoPrint";
+import EducationPrint from "../education/EducationPrint";
+import ExperiencePrint from "../experience/ExperiencePrint";
 
 function CVPreview({ generalInfo, education, experience }) {
     return(
         <div className="cv-preview">
             <GeneralInfoPrint fullName={generalInfo.fullName} email={generalInfo.email} phone={generalInfo.phone} adress={generalInfo.adress} />
+            <EducationPrint education={education} />
+            <ExperiencePrint experience={experience} />
         </div>
     )
 }
