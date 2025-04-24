71915617ba6fad31943d67a25227594c7db334f2
astuarykig <akvsevolod@icloud.com>
2025-04-24 22:19:03 +0500

    style: add display of period of education

--- src/components/education/EducationPrint.jsx
+++ src/components/education/EducationPrint.jsx
@@ -5,6 +5,7 @@ function EducationPrint({ education }) {
 
             {education.map(ed => (
                 <div key={ed.id} className="education-unit">
+                    <p>{ed.startDate.split('-').slice(0, 2).join('-') + ' – ' + ed.endDate.split('-').slice(0, 2).join('-')}</p>
                     <h3>{ed.degree}</h3>
                     <p>{ed.school}</p>
                 </div>
